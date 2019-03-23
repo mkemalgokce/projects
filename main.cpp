00105732018f527fd5555794f5320f358e1e8a2c
mkemalgokce <46056478+mkemalgokce@users.noreply.github.com>
2019-03-23 19:17:57 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include"kayit.h"
+using namespace std;
+
+int main(){
+    Kayit k1;
+
+
+}
---
+++ odev4a.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include"kayit.h"
+using namespace std;
+
+Kayit::Kayit(const string & kelime , const string & aciklama ){
+
+this->kelime = kelime;
+this->aciklama.push_back(aciklama);
+
+}
